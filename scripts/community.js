48041031aae838671656841d1fb04cd4429f0a3d
LuSSun <787688238@qq.com>
2017-10-18 11:35:17 +0800

    'update'

--- scripts/community.js
+++ scripts/community.js
@@ -1,35 +1,9 @@
 
-'use strict';
+(function($){
+	'use strict';
 jQuery(document).ready(function($) {
 	/*社区板块*/
-
-	/*搜索框*/
-	function searchTop(){
-		var searchBtn = $('.search_right .search');
-		var formHide = $('.form-hide');
-		var navTop = $('#com_navTop');
-		var closeBtn = $('.form-hide .fa-close')
-		searchBtn.click(function(){
-			$(this).hide();
-			navTop.hide()
-			formHide.show().stop(true,false).animate({
-				left:0
-			}, 300)
-			.find('.form_txt').focus();
-
-
-		})
-		closeBtn.click(function(){
-			
-			formHide.hide().stop(true,false).animate({
-				left:100,
-			}, 300)
-			searchBtn.show();
-			navTop.show();
-		})
-	}
-	searchTop();
-		
+	/*banner轮播图*/
 	$("#recent-works").carouFredSel({
 		prev:"#btn_prev",
 		next:"#btn_next",
@@ -175,27 +149,9 @@ jQuery(document).ready(function($) {
 	}
 	hoverShow();
 
-	/*回到顶部*/
-	function backTop(){
-		$("#sq_back").click(function(){
-			$('body').animate({'scrollTop':0},500);
-		});
-		$(window).scroll(function(){
-			var top = $(window).scrollTop();
-			if(top>1000){
-				$("#sq_back").stop(true,false).animate({'opacity':1}, 100);
-			}
-			else{
-				$("#sq_back").stop(true,false).animate({'opacity':0}, 100);
-
-			}
-		});
-	}
-	backTop();
-
-
-
 
 });
+})(jQuery)
+
 	
 
